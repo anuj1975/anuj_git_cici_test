2d0effaf550a3191fa7018407225f68dc40216a6
Anuj <anujatwork75@gmail.com>
2021-02-15 19:42:10 -0800

    first commit

---
+++ anuj_git_cicd/src/main/java/com/example/AnujGitCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnujGitCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnujGitCicdApplication.class, args);
+	}
+
+}
